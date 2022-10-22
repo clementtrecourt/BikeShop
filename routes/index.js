6374bd183b90dd83564d231249ac578da4b1bc5d
unknown <clement.trecourt@gmail.com>
2022-10-22 18:22:20 +0200

    added delete function backend

--- routes/index.js
+++ routes/index.js
@@ -5,13 +5,13 @@ let dataBike = [
     name: "BIKO45",
     image: "/images/bike-1.jpg",
     price: "536",
-    quantity: "4",
+    quantity: "1",
   },
   {
     name: "ZOOK7",
     image: "/images/bike-2.jpg",
     price: "642",
-    quantity: "2",
+    quantity: "1",
   },
   {
     name: "LIKO91",
@@ -24,8 +24,17 @@ let dataBike = [
 router.get("/", function (req, res, next) {
   res.render("index", { title: "Express", dataBike });
 });
+let basketBike = [];
 router.get("/shop", function (req, res, next) {
-  res.render("shop", { title: "Express", dataBike });
+  let bikeRef = req.query.ref;
+  basketBike.push(req.query);
+  console.log("one", basketBike);
+  res.render("shop", { title: "Express", basketBike, ref: bikeRef });
 });
-
+router.get("/delete", function (req, res, next) {
+  console.log(req.query.position);
+  basketBike.splice(req.query.position, 1);
+  console.log(basketBike);
+  res.render("shop", { basketBike });
+});
 module.exports = router;
